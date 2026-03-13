471b05d03689deaf60a185004370358a60a0e634
Kevin Gatera <kevingatera@gmail.com>
2026-03-13 08:37:03 -0400

    Improve cover viewer resolution and dismissal

--- lib/widgets/expanded_card.dart
+++ lib/widgets/expanded_card.dart
@@ -183,7 +183,7 @@ class _ExpandedCardState extends State<ExpandedCard> {
 
   String? get _viewerCoverUrl {
     final lib = context.read<LibraryProvider>();
-    return lib.getCoverUrl(_itemId, width: 2000);
+    return lib.getCoverUrl(_itemId, width: 2400);
   }
 
   bool get _isLocalCover => _coverUrl != null && _coverUrl!.startsWith('/');
